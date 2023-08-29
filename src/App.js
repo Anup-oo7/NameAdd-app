a8d25b6aaff97b2da43429086b7e6edcd71249e5
Anup-oo7 <srivastava8281@gmail.com>
2023-08-29 10:10:13 +0530

    my first commit in nameAdd-app

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import "./App.css";
+import styles from "./index.module.css";
+import NameInput from "./component/inputBox/Index";
+import Header from "./component/Header/Index";
+
+function App() {
+  const [listName, setListName] = useState([]);
+
+  let addList = (inputText) => {
+    if (inputText.length > 0) 
+    setListName([...listName, inputText])
+  else(alert('Type Name to add'))
+  }
+
+  const deleteHandler=(id)=>{
+    const newList =listName.filter((val,ind)=>{
+      return id!==ind
+    })
+    setListName(newList)
+  }
+  return (
+    <>
+      <Header>header</Header>
+      <div className="main-container">
+        <div className="centre-container">
+          <NameInput addList={addList} />
+          {listName.map((listItem, ind) => {
+            return <li key={ind} className={styles.listItem}>{listItem}<button className={styles.deleteItem} onClick={()=>deleteHandler(ind)}>Delete</button></li>;
+          })}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/inputBox/Index.js
@@ -0,0 +1,23 @@
+import React,{useState} from 'react'
+import styles from './index.module.css'
+function NameInput(props) {
+    const [inputText, setInputText]= useState("");
+
+    const onEnterClick=(e)=> {
+      e?.keyCode===13 && props.addList(inputText)  
+      e?.keyCode===13 && setInputText("");
+    }
+  return (
+    <div className="input-container">
+        <input type="text" value={inputText} className={styles.inputBox} placeholder='Enter your Name'
+           onChange={e=>{setInputText(e.target.value)}}  onKeyDown={onEnterClick}/>
+        <button className={styles.addBtn} onClick={()=>{props.addList(inputText)
+         setInputText("")}}>Add Name</button>
+
+      
+        
+    </div>
+  )
+}
+
+export default NameInput
